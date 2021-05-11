58c1359937545ed36620910d337a1176c59068c6
zuzalke <83219941+zuzalke@users.noreply.github.com>
2021-05-11 17:58:25 +0200

    Add files via upload

---
+++ Predkosci.m
@@ -0,0 +1,8 @@
+function [DQ] = Predkosci(Q, ParyObrotowe, ParyPostepowe, WymObrotowe, WymPostepowe,t, Fi_dt)
+
+%nie wrzucam tutaj liczenia FT jak w paczce, bo chyba to zrobiles w Fi_dt
+%jesli dobrze rozumiem (ale moge sie mylic)
+Fq=Jakobian(Q, ParyObrotowe, ParyPostepowe);
+DQ = -Fq \ Fi_dt(Q,t);
+
+end
