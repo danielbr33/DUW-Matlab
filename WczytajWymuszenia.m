0a13ddb16f55f999345eaa54877adfe86c3a307b
zuzalke <83219941+zuzalke@users.noreply.github.com>
2021-05-16 16:58:47 +0200

    Add files via upload

--- WczytajWymuszenia.m
+++ WczytajWymuszenia.m
@@ -7,16 +7,16 @@ for i=1:number
      czlon2 = dane(2);
      L = (dane(3)-dane(5))^2+(dane(4)-dane(6))^2;
      L = sqrt(L);
-     L=L/2;
-     if i==1
-         L = 0.4472;
-     end
-     if i==2
-         L = 0.6325;
-     end
-     a = 0.05;
-     w = 1;
-     fi = 0;
+     %L=L/2;
+     %if i==1
+     %    L = 0.447;
+     %end
+     %if i==2
+     %    L = 0.632;
+     %end
+     a = dane(7);
+     w = dane(8);
+     fi = dane(9);
      [Q, UkladyWspolrzednych, ParyObrotowe, ParyPostepowe] = ReadStartData;
      punktA = [dane(3)  dane(4)]';
      punktB = [dane(5)  dane(6)]';
@@ -25,16 +25,6 @@ for i=1:number
      sb = punktB - uklad2;
      sa = punktA - uklad1;
      sa = sa'; sb =sb';
-     if i==1
-         a=0.1;
-         w=3;
-         fi=0;
-     end
-     if i==2
-         a=0.15;
-         w=2;
-         fi=0;
-     end
      
      kat = atan2(dane(6)-dane(4), dane(5)-dane(3));  % do okreslenie przemieszczenia w ukladzie c4 i c6 
      parametryFunkcji(i,:) = [czlon1 czlon2 L, a, w, fi, kat, sa, sb];
