dfd26a688fac4b88cb684dba977a78c4d6ea32c3
Daniel <danielbr1999@o2.pl>
2021-06-12 21:34:47 +0200

    wykresy błędów

--- calkuj_ode45.m
+++ calkuj_ode45.m
@@ -11,17 +11,34 @@ dq0 = zeros(length(q0), 1);
 t0=0;
 Y0=[q0;dq0];
 
-[t,Y]=ode45(@H,[t0,tk],Y0);
+[t,Y]=ode45(@H,t0:0.1:tk,Y0);
 
 %toc
-n=size(Y,1);
+n=size(Y,1); % Liczba wierszy w tablicy wyników
+err_F(n)=0; err_dF(n)=0; err_d2F(n)=0; % Niespe³nienie wiêzów - deklaracja d³ugoœci wektorów 
 m = length(q0);
-qK=Y(n,1:m)';
-dqK=Y(n,(m+1):(2*m))';
-q_dq=[qK dqK]
+for i=1:n
+    q=Y(i,1:m)'; dq=Y(i,m+1:2*m)';  % Po³o¿enia i prêdkoœci w i-tym stablicowanym kroku
+    FI=Fi(q,t,ParyObrotowe, ParyPostepowe, WymuszeniaParametry);
+    FQ=Jakobian(q, ParyObrotowe, ParyPostepowe, WymuszeniaParametry);
+    G=Gamma(q,dq, ParyObrotowe, ParyPostepowe, WymuszeniaParametry,t);
+    M = Macierz;
+    Q = Sily(q, dq, ParyPostepowe, Masy);
+    
+    A=[M,FQ';FQ,zeros(28,28)];     
+    b=[Q;G];                   
+    x=A\b;                           
+    d2q=x(1:m,1);
 
-%dziwny ruch - nwm czy do konca dobrze
-F=Fi(qK,t,ParyObrotowe, ParyPostepowe, WymuszeniaParametry);
-norm_F=norm(F);
+    % Obliczenie miar rozerwania wiêzów
+    err_F(i)=norm(FI);               % B³¹d wiêzów dla po³o¿eñ
+    err_dF(i)=norm(FQ*dq);          % B³¹d wiêzów dla prêdkoœci
+    err_d2F(i)=norm(FQ*d2q-G);      % B³¹d wiêzów dla przyspieszeñ
+end
+plot(t,err_F,'b','LineWidth',2); hold on;
+plot(t,err_dF,'g','LineWidth',2);
+plot(t,err_d2F,'r','LineWidth',2);
+grid;
+legend('Po³o¿enia','Prêdkoœci','Przyspieszenia'); hold off;
 
 end
